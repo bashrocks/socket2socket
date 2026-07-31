325414cb631751d1783aecf6b963150896a0ae94
bashrocks <bashrocks@proton.me>
2026-07-31 17:37:34 -0400

    client to server message established

--- com/irc/IRCClient.java
+++ com/irc/IRCClient.java
@@ -1,6 +1,9 @@
 package com.irc;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -8,6 +11,8 @@ public class IRCClient {
 	
 	// static String serverAddress;
 	static Socket connection;
+	static BufferedReader in;
+	static BufferedWriter out;
 
 	public IRCClient() {
 		// TODO Auto-generated constructor stub
@@ -17,6 +22,7 @@ public class IRCClient {
 		try {
 			connection = new Socket(address,port);
 			System.out.println("Connected to server " + address);
+			out = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -27,11 +33,23 @@ public class IRCClient {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		QuitProgram quitChecker = new QuitProgram();
-		Thread quitThread = new Thread(quitChecker);
-		quitThread.start();
+		// QuitProgram quitChecker = new QuitProgram();
+		// Thread quitThread = new Thread(quitChecker);
+		// quitThread.start();
 		connect("localhost",1970);
+
+        try {
+		while (true) {
+			out.write("Hello World!");
+            out.newLine();
+            out.flush();
+
+            Thread.sleep(200);
+        }
+        } catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- com/irc/IRCServer.java
+++ com/irc/IRCServer.java
@@ -1,6 +1,9 @@
 package com.irc;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,6 +11,8 @@ public class IRCServer {
 	
 	static ServerSocket listener;
 	static Socket connection;
+	static BufferedReader in;
+	static BufferedWriter out;
 
 	public IRCServer() {
 		// TODO Auto-generated constructor stub
@@ -37,20 +42,33 @@ public class IRCServer {
 			connection = listener.accept();
 			listener.close();
 			System.out.println("Connected with " + connection.getInetAddress());
+			getMessage();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void getMessage() {
+		try {
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		
+			String line = null;
+	        while ((line = in.readLine()) != null) {
+	        	System.out.println(line);
+	        }
+        } catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		QuitProgram quitChecker = new QuitProgram();
 		Thread quitThread = new Thread(quitChecker);
 		quitThread.start();
 		
 		listen(1970);
-
 	}
 
 }
