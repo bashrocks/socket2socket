6ed898f89a680a32b7c439e2f8b9bf11c9b8799e
bashrocks <bashrocks@proton.me>
2026-07-31 20:46:19 -0400

    listener/connection classes

---
+++ com/irc/Connection.java
@@ -0,0 +1,20 @@
+package com.irc;
+
+import java.net.Socket;
+
+public class Connection implements Runnable {
+	Socket socket;
+
+	public Connection(Socket socket) {
+		// TODO Auto-generated constructor stub
+		this.socket = socket; 
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Connected with " + socket.getInetAddress());
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ com/irc/Listener.java
@@ -0,0 +1,51 @@
+package com.irc;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Listener implements Runnable {
+
+	ServerSocket listener;
+	Socket connection;
+	int port;
+	
+	public Listener(int port) { 
+		// TODO Auto-generated constructor stub
+		this.port = port;
+		try {
+			this.listener = new ServerSocket(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void closeAndOpen() {
+		try {
+			listener.close();
+			this.listener = new ServerSocket(port);
+			System.out.println("Listening on port " + listener.getLocalPort());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			do {
+				System.out.println("Listening on port " + listener.getLocalPort());
+				Thread connection = new Thread(new Connection(listener.accept()));
+				connection.start();
+				closeAndOpen();
+			} while(true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
