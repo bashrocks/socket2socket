c8ccf0ebbf6c527ba91851aa0fcbf471f0b7f4fe
bashrocks <bashrocks@proton.me>
2026-07-31 21:41:27 -0400

    code based on stackoverflow, readability tweaks
    https://stackoverflow.com/questions/10069059/is-it-possible-to-run-a-socket-server-and-socket-client-on-the-same-machine

--- com/irc/IRCClient.java
+++ com/irc/IRCClient.java
@@ -12,12 +12,43 @@ public class IRCClient {
 	public IRCClient() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public static void startSender(String message) {
+        Thread sender = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Socket s = new Socket("localhost", 60010);
+                    BufferedWriter out = new BufferedWriter(
+                            new OutputStreamWriter(s.getOutputStream()));
+
+                    while (true) {
+                        out.write(message);
+                        out.newLine();
+                        out.flush();
+
+                        Thread.sleep(1000);
+                    }
+
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        sender.start();
+    }
 
 	public static void main(String[] args) {
 		// QuitProgram quitChecker = new QuitProgram();
 		// Thread quitThread = new Thread(quitChecker);
 		// quitThread.start();
-		
+		String message = "Hello World!";
+		if(args.length>0) { message = args[0]; }
+		startSender(message);
 	}
 
 }
--- com/irc/IRCServer.java
+++ com/irc/IRCServer.java
@@ -13,9 +13,33 @@ public class IRCServer {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public static void startServer() {
+		
+		Thread server = new Thread() {
+            @Override
+            public void run() {
+                ServerSocket ss;
+                try {
+                    ss = new ServerSocket(60010);
+
+                    Socket s = ss.accept();
+
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(s.getInputStream()));
+                    String line = null;
+                    while ((line = in.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+		server.start();
+    }
 
 	public static void main(String[] args) {
-		
+		startServer();
 	}
 
 }
