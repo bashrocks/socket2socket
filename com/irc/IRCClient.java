09dc28bcdd5dde042e456a1b93465e9baac0b369
bashrocks <bashrocks@proton.me>
2026-07-31 21:26:13 -0400

    let's start from scratch for a bit

--- com/irc/IRCClient.java
+++ com/irc/IRCClient.java
@@ -8,48 +8,16 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class IRCClient {
-	
-	// static String serverAddress;
-	static Socket connection;
-	static BufferedReader in;
-	static BufferedWriter out;
 
 	public IRCClient() {
 		// TODO Auto-generated constructor stub
-	}
-	
-	public static void connect(String address,int port) {
-		try {
-			connection = new Socket(address,port);
-			System.out.println("Connected to server " + address);
-			out = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 	public static void main(String[] args) {
 		// QuitProgram quitChecker = new QuitProgram();
 		// Thread quitThread = new Thread(quitChecker);
 		// quitThread.start();
-		connect("localhost",1970);
-
-        try {
-		while (true) {
-			out.write("Hello World!");
-            out.newLine();
-            out.flush();
-
-            Thread.sleep(200);
-        }
-        } catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 
 }
--- com/irc/IRCServer.java
+++ com/irc/IRCServer.java
@@ -8,69 +8,14 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class IRCServer {
-	
-	static ServerSocket listener;
-	static Socket connection;
-	static BufferedReader in;
-	static BufferedWriter out;
 
 	public IRCServer() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public static void listen(int port) {
-		Thread listenThread = new Thread() {
-			@Override
-			public void run() {
-				while(true) {
-					try {
-						listener = new ServerSocket(port);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					System.out.println("Listening on port " + listener.getLocalPort());
-					connect();
-				}
-			}
-		};
-		listenThread.start();
-	}
-	
-	public static void connect() {
-		try {
-			connection = listener.accept();
-			listener.close();
-			System.out.println("Connected with " + connection.getInetAddress());
-			getMessage();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public static void getMessage() {
-		try {
-			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		
-			String line = null;
-	        while ((line = in.readLine()) != null) {
-	        	System.out.println(line);
-	        }
-        } catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
 	public static void main(String[] args) {
-		QuitProgram quitChecker = new QuitProgram();
-		Thread quitThread = new Thread(quitChecker);
-		quitThread.start();
 		
-		Listener listen = new Listener(1970);
-		Thread listenThread = new Thread(listen);
-		listenThread.start();
 	}
 
 }
